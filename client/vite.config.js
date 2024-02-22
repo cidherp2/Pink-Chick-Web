cf906bb1fbc9e6e602e43de5f4ca01621bb54943
Alejandro Larios <alexlarios70@hotmail.com>
2024-02-22 00:20:24 -0600

    vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   base:'/',
   server:{
     proxy:{
       "/pink/":"http://localhost:3001/" ,
     }, 
   },
-  plugins: [react()],
+  
 })
