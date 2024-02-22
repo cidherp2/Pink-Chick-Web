685435d4cbf92c33686c977a4efbae94ef2297b4
Alejandro Larios <alexlarios70@hotmail.com>
2024-02-22 03:44:57 -0600

    cambio proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:'/',
-  server:{
-    proxy:{
-      "/pink/":"https://pinkhicks-b921d828c404.herokuapp.com/" ,
-    }, 
-  },
-  
+  plugins: [react()], 
 })
